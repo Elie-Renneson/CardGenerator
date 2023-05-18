80e22f24fe45b0ae77f203d0f519f0f8d517a378
Elie Renneson-Boutard <elie@Elies-Air-001.lan>
2023-05-18 12:07:25 +0200

    updating

--- home/models.py
+++ home/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Phrase(models.Model):
     texte = models.CharField(max_length=255)
     icon = models.CharField(max_length=255, default="Pinterest.png")
+
+    def __str__(self):
+        return f"{self.texte}"
